476b58a8f6813f83243826e6e7ce36278283785f
caotien <159750613+xuantien37@users.noreply.github.com>
2024-10-20 19:59:57 +0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+// Daniel Shiffman
+// http://codingtra.in
+// https://youtu.be/CKeyIbT3vXI
+
+const fireworks = [];
+let gravity;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB);
+  gravity = createVector(0, 0.2);
+  stroke(255);
+  strokeWeight(4);
+  background(0);
+}
+
+function draw() {
+  colorMode(RGB);
+  background(0, 0, 0, 25);
+
+  if (random(1) < 0.04) {
+    fireworks.push(new Firework());
+  }
+
+  for (let i = fireworks.length - 1; i >= 0; i--) {
+    fireworks[i].update();
+    fireworks[i].show();
+
+    if (fireworks[i].done()) {
+      fireworks.splice(i, 1);
+    }
+  }
+}
